fcee47ef79438b62d9b218ee84cc27f8e06079ae
Shimul Sutradhar <56151176+shimulSutradhar@users.noreply.github.com>
2020-06-02 08:16:20 +0600

    Create Task 7: Greatest Common Divisor.cpp

---
+++ Task 7: Greatest Common Divisor.cpp
@@ -0,0 +1,17 @@
+/*
+    author: Shimul Sutradhar
+    ID    : 191-15-12614
+*/
+#include<iostream>
+using namespace std;
+int gcd(int a, int b){
+    if(a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << '\n';
+    return 0;
+}
